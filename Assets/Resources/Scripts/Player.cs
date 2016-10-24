e9f9c3a15b59fe01734b4d25a5eef12c233b6b0e
flanbel <kbc15b12@stu.kawahara.ac.jp>
2016-10-24 13:11:34 +0900

    Assets追加

---
+++ Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    CharacterController Characon;
+    [SerializeField]
+    float AddGavity = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        //キャラクターコントローラーのコンポーネントを取得
+        Characon = this.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Characon.Move(this.transform.TransformDirection(Vector3.forward));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Characon.Move(this.transform.TransformDirection(Vector3.back));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Characon.Move(this.transform.TransformDirection(Vector3.left));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Characon.Move(this.transform.TransformDirection(Vector3.right));
+        }
+
+        //地面についていないなら
+        if (!Characon.isGrounded)
+        {
+            AddGavity += Physics.gravity.y * Time.deltaTime;
+        }else
+        {
+            AddGavity = 0.0f;
+        }
+
+        Characon.Move(new Vector3(0, AddGavity, 0) * Time.deltaTime);
+    }
+}
