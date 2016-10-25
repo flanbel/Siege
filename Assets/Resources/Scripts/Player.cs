90886b130fd8ead16acef7de1f6dd0b233473957
flanbel <kbc15b12@stu.kawahara.ac.jp>
2016-10-25 11:59:54 +0900

    オンラインでのプレイヤーの動きを補完するようにした

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -1,46 +1,56 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class Player : MonoBehaviour {
-
+public class Player : NetworkBehaviour
+{
+    //キャラクターコントローラー
     CharacterController Characon;
+    //ネットワークのオブジェクトを識別する。
+    NetworkIdentity Identity;
     [SerializeField]
     float AddGavity = 0.0f;
-
 	// Use this for initialization
 	void Start () {
         //キャラクターコントローラーのコンポーネントを取得
         Characon = this.GetComponent<CharacterController>();
-	}
+        //コンポーネント取得
+        Identity = this.GetComponent<NetworkIdentity>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.W))
+        //ローカルなオブジェクト(ローカル側で生成された)なら
+        if (Identity.isLocalPlayer)
         {
-            Characon.Move(this.transform.TransformDirection(Vector3.forward));
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            Characon.Move(this.transform.TransformDirection(Vector3.back));
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            Characon.Move(this.transform.TransformDirection(Vector3.left));
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            Characon.Move(this.transform.TransformDirection(Vector3.right));
-        }
+            if (Input.GetKey(KeyCode.W))
+            {
+                Characon.Move(this.transform.TransformDirection(Vector3.forward));
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                Characon.Move(this.transform.TransformDirection(Vector3.back));
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                Characon.Move(this.transform.TransformDirection(Vector3.left));
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                Characon.Move(this.transform.TransformDirection(Vector3.right));
+            }
 
-        //地面についていないなら
-        if (!Characon.isGrounded)
-        {
-            AddGavity += Physics.gravity.y * Time.deltaTime;
-        }else
-        {
-            AddGavity = 0.0f;
-        }
+            //地面についていないなら
+            if (!Characon.isGrounded)
+            {
+                AddGavity += Physics.gravity.y * Time.deltaTime;
+            }
+            else
+            {
+                AddGavity = 0.0f;
+            }
 
-        Characon.Move(new Vector3(0, AddGavity, 0) * Time.deltaTime);
+            Characon.Move(new Vector3(0, AddGavity, 0) * Time.deltaTime);
+        }
     }
 }
