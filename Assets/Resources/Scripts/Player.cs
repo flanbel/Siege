2673ce2e3b3b2020ffc24718ccddc80b9a65848a
flanbel <kbc15b12@stu.kawahara.ac.jp>
2016-10-26 18:29:08 +0900

    東北ずん子追加

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -6,12 +6,17 @@ public class Player : NetworkBehaviour
 {
     //キャラクターコントローラー
     CharacterController Characon;
+    //アニメーター
+    public Animator Ani;
     //ネットワークのオブジェクトを識別する。
     NetworkIdentity Identity;
     [SerializeField]
     float AddGavity = 0.0f;
     [SerializeField]
-    float speed = 3;
+    float Speed = 3;
+    bool Jamp = false;
+
+    Ray ray;
 
     //ローカルで生成されたなら実行
     public override void OnStartLocalPlayer()
@@ -31,6 +36,8 @@ public class Player : NetworkBehaviour
         Characon = this.GetComponent<CharacterController>();
         //コンポーネント取得
         Identity = this.GetComponent<NetworkIdentity>();
+
+        Ani = this.GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
@@ -38,37 +45,64 @@ public class Player : NetworkBehaviour
         //ローカルなオブジェクト(ローカル側で生成された)なら
         if (Identity.isLocalPlayer)
         {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Ani.SetTrigger("WalkTrigger");
+            }
+
             //WASD機能
             if (Input.GetKey(KeyCode.W))
             {
-                Characon.Move(this.transform.TransformDirection(Vector3.forward) * Time.deltaTime * speed);
+                Characon.Move(this.transform.TransformDirection(Vector3.forward) * Time.deltaTime * Speed);
             }
             if (Input.GetKey(KeyCode.S))
             {
-                Characon.Move(this.transform.TransformDirection(Vector3.back) * Time.deltaTime * speed);
+                Characon.Move(this.transform.TransformDirection(Vector3.back) * Time.deltaTime * Speed);
             }
             if (Input.GetKey(KeyCode.A))
             {
-                Characon.Move(this.transform.TransformDirection(Vector3.left) * Time.deltaTime * speed);
+                Characon.Move(this.transform.TransformDirection(Vector3.left) * Time.deltaTime * Speed);
             }
             if (Input.GetKey(KeyCode.D))
             {
-                Characon.Move(this.transform.TransformDirection(Vector3.right) * Time.deltaTime * speed);
+                Characon.Move(this.transform.TransformDirection(Vector3.right) * Time.deltaTime * Speed);
             }
             //マウスで回転
             transform.localEulerAngles += new Vector3(0.0f, Input.GetAxis("Mouse X"));
 
-            //地面についていないなら
-            if (!Characon.isGrounded)
-            {
-                AddGavity += Physics.gravity.y * Time.deltaTime;
-            }
-            else
+            //重力を加算
+            AddGavity += Physics.gravity.y * Time.deltaTime;
+
+            ray = new Ray(transform.position, Vector3.down);
+
+            //レイが当たっているか、地上に当たっているか
+            if (Physics.Raycast(ray, 0.6f) || Characon.isGrounded)
             {
                 AddGavity = 0.0f;
+                Jamp = false;
+            }
+
+            if (!Jamp)
+            {
+                //スペースキーでジャンプ
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    Jamp = true;
+                }
+            }
+
+            if (Jamp)
+            {
+                Characon.Move(new Vector3(0, 10, 0) * Time.deltaTime);
             }
 
             Characon.Move(new Vector3(0, AddGavity, 0) * Time.deltaTime);
+
         }
+    }
+
+    void FixedUpdate()
+    {
+        
     }
 }
