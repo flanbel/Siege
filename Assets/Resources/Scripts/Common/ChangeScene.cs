ea7a5a3626d2f7c6790da9862b86643d2ae59b61
flanbel <kbc15b12@stu.kawahara.ac.jp>
2016-11-25 12:31:27 +0900

    hennkou

---
+++ Assets/Resources/Scripts/Common/ChangeScene.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+    //次のシーン
+    public string nextScene;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+    public void ChengeScene()
+    {
+        //シーン切り替え
+        SceneManager.LoadScene(nextScene);
+    }
+}
