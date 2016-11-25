c416fd5f7f60608b7f6ba2f04e27e00300cfe323
a <a>
2016-11-25 10:31:36 +0900

    プレイヤーのプレハブ追加

---
+++ Assets/Resources/Scripts/Game/NormalPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class NormalPlayer : PlayerBase
+{
+    new void Start()
+    {
+        base.Start();
+    }
+    new public void Update()
+    {
+        base.Update();
+    }
+}
