1a0267652bb523180d86de65a29dae1065d3bf77
flanbel <kbc15b12@stu.kawahara.ac.jp>
2017-02-02 10:33:49 +0900

    playermove追加？

--- Assets/Resources/Scripts/test/Player_Move.cs
+++ Assets/Resources/Scripts/test/Player_Move.cs
@@ -6,30 +6,40 @@ public class Player_Move : NetworkBehaviour
 {
 
     //Primitive型のみ許される
-    [SyncVar]   //ホストから全クライアントへ送られる
+    [SyncVar]
     private Vector3 dir;    //移動量
+    [SyncVar]
+    private Vector3 pos;    //移動した先
+    [SyncVar]
+    private bool move = false;      //移動フラグ
 
     void Update()
     {
+        if(move == true)
         LerpPosition();
     }
 
     //ポジション補間用メソッド
     void LerpPosition()
     {
-        //補間対象は相手プレイヤーのみ
-        //if (!isLocalPlayer)
+        //サーバーなら
+        if (isServer)
         {
-            //Lerp(from, to, 割合) from〜toのベクトル間を補間する
-            transform.position += dir;
+            //移動
+            transform.localPosition += dir;
+            pos = transform.localPosition;
+        }else if (isLocalPlayer)
+        {
+            transform.localPosition = pos;
         }
     }
     //クライアントからホストへ、Position情報を送る
     [Command]
-    void CmdProvidePositionToServer(Vector3 pos)
+    void CmdProvidePositionToServer(Vector3 vec)
     {
         //サーバー側が受け取る値
-        dir = pos;
+        //サーバーへ移動量を送信
+        dir = vec;
     }
 
     //クライアントのみ実行される
@@ -37,11 +47,13 @@ public class Player_Move : NetworkBehaviour
     //位置情報を送るメソッド
     public void Transmit(Vector3 vec)
     {
+        move = false; 
         //ローカル（操作している）かつ
         //移動量がある
-        if (isLocalPlayer || vec.magnitude > 0)
+        if (isLocalPlayer && vec.magnitude > 0)
         {
             CmdProvidePositionToServer(vec);
+            move = true;
         }
     }
 }
